ecf8e41626a8578c70ff149e537e272f1da61860
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-04 11:48:59 -0300

    typescript

---
+++ index.js
@@ -0,0 +1,6 @@
+export const sum = (a, b) => {
+    return a + b;
+}
+
+sum(["Osvaldo"], 123, "Barata");
+sum(1,2);
--- index.ts
+++ index.ts
@@ -1,12 +1,60 @@
-const add = (a: number, b: number): string => {
-    const sum = a + b;
-    return `A soma é: ${sum}`
+// tipos primitivos
+// string
+// number
+// boolean
+
+let age:number;
+
+//age = "Osvaldo";
+
+// [] lista de alguma coisa
+
+export const setState = (state: string[]) => {
+    console.log(state);
 }
 
-const printNames = (nameList: string[]) => {
-    nameList.forEach((name) => console.log(name));
+let nameList = ["Osvaldo", "Petrucio", "Rodolfo"];
+nameList.map((name) => name);
+
+setState([...nameList, "Ferdinando"]);
+
+//Inferência
+// Só vou atribuir tipagem quando a inferência falha e atribuir o tipo que eu quero
+
+// Casos que a inferência falha (casos que eu preciso apontar os tipos)
+
+// Parâmetros de funções (props estarão inclusas nessa regra)
+export const sum = (a: number, b: number) => {
+    return a + b;
 }
 
-// string, number e boolean
+//Lista vazias 
+let ageList: number[] = [];
 
-add(10, 10);
+//Variação de tipo (precisaremos apontar tipagem)
+// let user: string | null = null;
+
+//Interface: é o um recurso para construirmos tipagens de objeto (descrever um objeto)
+
+interface IUser{
+    name: string;
+    email: string;
+    age: number;
+}
+
+let user: IUser | null = null;
+
+export const setUserList = (state: IUser[]) => {
+    console.log(state)
+}
+
+let userList: IUser[] = [];
+
+const newUser = { name: "Osvaldinho", email: "email@email.com", age: 32};
+setUserList([...userList, newUser]);
+
+userList.filter(user => user.name !== "Percival")
+
+const midAge = userList.reduce((prevValue, user) => {
+    return (prevValue + user.age)/userList.length;
+}, 0);
